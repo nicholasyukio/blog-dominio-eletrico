62973f67babaf3641165e69a8de61ee55665ac55
Nicholas Yukio <nicholasyukio@gmail.com>
2025-11-10 00:14:43 -0300

    Disabling eslint rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    // ⚠️ Warning: this allows production builds to succeed even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
-
 export default nextConfig;
